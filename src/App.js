40271062405d5e4fcc89ad494300d8aa956e81e2
Gagan Aptagiri <gagan.aptagiri@gmail.com>
2021-08-10 12:14:12 +0530

    Debouncing search for national sports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react';
+import nationalSports from './national-sports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	console.log('render');
+	const [search, setSearch] = useState('');
+	const [filteredNationalSports, setFilteredNationalSports] =
+		useState(nationalSports);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			const filter = nationalSports.filter((sport) => {
+				return sport['national sport']
+					.toLowerCase()
+					.includes(search.toLowerCase());
+			});
+
+			setFilteredNationalSports(filter);
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	}, [search]);
+
+	return (
+		<Fragment>
+			<input value={search} onChange={(e) => setSearch(e.target.value)} />
+			<ul>
+				{filteredNationalSports &&
+					filteredNationalSports.map((sport) => {
+						return <li key={sport.country}>{sport['national sport']}</li>;
+					})}
+			</ul>
+		</Fragment>
+	);
+};
 
 export default App;
